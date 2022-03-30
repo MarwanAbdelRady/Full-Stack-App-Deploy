820333dc88ca855994fe8f664117afc9ddcb7b9a
MarwanAbdelRady <marwanabdelrady97@gmail.com>
2022-03-30 15:21:50 +0200

    Wm-en003

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   appName: "Udagram",
   // apiHost: 'http://localhost:8080/api/v0'
-  apiHost: "http://wm-env.eba-b5cmpuxg.us-east-1.elasticbeanstalk.com/api/v0",
+  apiHost: "http://Wm-env.eba-b5cmpuxg.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
