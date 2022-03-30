8736fef8cca10f5a388c4687fd803e6956a28a54
MarwanAbdelRady <marwanabdelrady97@gmail.com>
2022-03-30 16:30:13 +0200

    onee

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,7 +1,7 @@
 
-eb init Wm-env --platform node.js --region us-east-1 
-eb use Wm-env
-eb deploy Wm-env
+eb init Udagram-env-1 --platform node.js --region us-east-1 
+eb use Udagram-env-1
+eb deploy Udagram-env-1
 
 
 eb setenv AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" AWS_DEFAULT_REGION="$AWS_DEFAULT_REGION" POSTGRES_USERNAME="$POSTGRES_USERNAME" POSTGRES_PASSWORD="$POSTGRES_PASSWORD" POSTGRES_DB="$POSTGRES_DB" PORT="$PORT" POSTGRES_HOST="$POSTGRES_HOST" AWS_REGION="$AWS_REGION" AWS_PROFILE="$AWS_PROFILE" AWS_BUCKET="$AWS_BUCKET" URL="$URL" DB_PORT="$DB_PORT" JWT_SECRET="$JWT_SECRET" --profile default
